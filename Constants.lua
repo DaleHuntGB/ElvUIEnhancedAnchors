ac86c30f17a2d6cd731036deab68a231c48f23dc
Dale Hunt <dalehunt@gmail.com>
2026-04-17 12:35:38 +0100

    This isn't needed anymore.

--- Constants.lua
+++ Constants.lua
@@ -1,20 +1,6 @@
 local Private = select(2, ...)
 Private.E = unpack(ElvUI)
 
-Private.Movers = {}
-
-Private.Movers = {
-    ["Player"] = "ElvUF_PlayerMover",
-    ["PlayerCastBar"] = "ElvUF_PlayerCastbarMover",
-    ["Target"] = "ElvUF_TargetMover",
-    ["TargetCastBar"] = "ElvUF_TargetCastbarMover",
-    ["TargetTarget"] = "ElvUF_TargetTargetMover",
-    ["Focus"] = "ElvUF_FocusMover",
-    ["FocusCastBar"] = "ElvUF_FocusCastbarMover",
-    ["FocusTarget"] = "ElvUF_FocusTargetMover",
-    ["Pet"] = "ElvUF_PetMover"
-}
-
 function Private:SetMovers(isEnabled, moverName, moverLayout)
     EventUtil.ContinueOnAddOnLoaded("ElvUI", function()
         if isEnabled then
